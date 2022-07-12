44c65c0360bde2c2e6a3301d146c9488523be574
DiegoPaulinoCasa <diegopaulino023@gmail.com>
2022-07-12 19:17:59 -0300

    voltando para o commit 891ba11

--- app.js
+++ app.js
@@ -2,5 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicacao está se conectando ao banco de dados")
 //mais um console log de teste;
 console.log("O Diego fez esse commit sem você ver na aula!");
-console.log("Enviando para o github");
-
